64cb234cb3d05217ddc66707997a6a9abe9c1721
Eunsil-Jeong <daramchu@naver.com>
2019-04-20 10:53:58 +0900

    결과화면

--- app/src/main/java/com/tjeit/a20190414_finaltest/MainActivity.java
+++ app/src/main/java/com/tjeit/a20190414_finaltest/MainActivity.java
@@ -1,35 +1,44 @@
 package com.tjeit.a20190414_finaltest;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText inputWeight;
-    EditText inputTall;
-    Button btnResult;
 
+
+    private EditText heightEdt;
+    private EditText WeightEdt;
+    private Button okBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
 
-        inputWeight = findViewById(R.id.inputWeight);
-        inputTall = findViewById(R.id.inputTall);
-        btnResult = findViewById(R.id.btnResult);
+    void setValues(){
+        // 초기데이터 
+    }
 
-        btnResult.setOnClickListener(new View.OnClickListener() {
+    void setupEvents(){
+        okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userWeight = inputWeight.getText().toString();
-                String userTall =  inputTall.getText().toString();
-
 
             }
         });
     }
+
+    void bindViews() {
+        this.okBtn = (Button) findViewById(R.id.okBtn);
+        this.WeightEdt = (EditText) findViewById(R.id.WeightEdt);
+        this.heightEdt = (EditText) findViewById(R.id.heightEdt);
+    }
+
 }
